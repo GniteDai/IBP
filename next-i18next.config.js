bf12a49df6e854cbe8582cfe32a486da3f4e0d26
Dai <gnitekk@gmail.com>
2022-10-30 20:51:52 +0800

    [Fix]

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,8 @@
+const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'zh',
-    locales: ['en', 'zh']
+    locales: ['en', 'zh'],
+    localePath: path.resolve('./public/locales')
   }
 };
